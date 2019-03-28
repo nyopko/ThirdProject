0ae8e323de45f55b10139032b824cc1ab9f2f9a2
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-03-28 14:55:13 -0400

    Linked bootstrap if we choose to use it, created test navbar.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
+      <Nav></Nav>
       <Header></Header>
       <Body></Body>
       <Footer></Footer>
