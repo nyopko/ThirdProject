f407ebb3c176dde9eb199b969415db5af992fb56
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-03-28 14:45:39 -0400

    Tested Body component with demo text, added to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Body from "./components/Body";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
       <Header></Header>
+      <Body></Body>
+      </div>
     );
   }
 }
---
+++ src/components/Body/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+function Body() {
+  return (
+        <p> Body Here</p>
+    );
+}
+
+export default Body;
