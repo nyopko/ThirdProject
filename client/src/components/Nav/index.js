e89f70685fe94515d29ded65d06598943c0690d1
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-03-30 13:50:01 -0400

    Added nav bar from materialize

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,22 +3,12 @@ import "./style.css";
 
 function Nav() {
     return <div className="nav">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="google.com">Space App</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="google.com">Link 1 <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link active" href="google.com">Link 2</a>
-      
-      
+        <nav>
+            <div class="nav-wrapper">
+                <a href="google.com" class="brand-logo">Space!</a>
+            </div>
+        </nav>
     </div>
-  </div>
-</nav>
-    
-    </div>;
 }
 
 export default Nav;
