9146813d8690f8df7dea19032b1763c811374b90
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-04-02 12:52:48 -0400

    Added jsx for footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,7 +3,22 @@ import './style.css';
 
 function Footer() {
   return (
-        <p>Footer here.</p>
+    <div class="footer">
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <p class="grey-text text-lighten-4">footer content</p>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+            © 2019 Copyright Text
+            </div>
+        </div>
+    </footer>
+</div>
     );
 }
 
