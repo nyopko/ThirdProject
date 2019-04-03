f84612721dcf338b532d3fa309288a8af0a14ffe
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-04-03 11:51:38 -0400

    Created planet route, need  image and content body card

---
+++ client/src/pages/Planets.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import TotalWrapper from '../components/TotalWrapper';
+import Header from '../components/Header';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="planetsPage">
+      <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
