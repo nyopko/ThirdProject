982a5eae7f6be5396d789c3f1af58a87759cbbf1
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-04-03 12:24:43 -0400

    picture enlarged and in place on planet route, mapping now, need to fix footing space error

--- client/src/components/PlanetContent/index.js
+++ client/src/components/PlanetContent/index.js
@@ -3,19 +3,21 @@ import './style.css';
 import solarsystem from '../img/solarsystem.jpg';
 
 function PlanetContent() {
-  return (
-    <div className="row">
-      <div className="col s12">
-        <div className="card-panel">
-          <div className="row">
-            <div className="col s4 m2">
-              <img src={solarsystem} alt="planets"className="responsive-img"/>
-                                </div>
-                                </div>
-                                </div>
-                                </div>
-              </div>
-            );
+    return (
+        <div className="PlanetContent">
+            <div className="row">
+                <div className="col s12">
+                    <div className="card-panel">
+                        <div className="row">
+                            <div className="col s4 m2">
+                                <img src={solarsystem} alt="planets" className="planetPic" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default PlanetContent;
