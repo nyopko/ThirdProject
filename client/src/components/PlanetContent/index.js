fd9721fb853feee74d286660cfcbeaa83769870d
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-04-03 12:16:35 -0400

    Changed location of img folder in order to correct 'outside src import error' uploaded space image, still need to style/resize and map

--- client/src/components/PlanetContent/index.js
+++ client/src/components/PlanetContent/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import solarsystem from '../img/solarsystem.jpg';
 
 function PlanetContent() {
   return (
@@ -8,16 +9,12 @@ function PlanetContent() {
         <div className="card-panel">
           <div className="row">
             <div className="col s4 m2">
-              <img src="https://media.npr.org/assets/img/2019/02/15/oppyconcept-7470fec47830d38c328ddf46b81eeeabc8e36c7c-s1100-c15.jpg" alt="rover"className="responsive-img"/>
+              <img src={solarsystem} alt="planets"className="responsive-img"/>
                                 </div>
-              <div className="col s8 m10">
-                <p>Links and Content for the mars rover goes here
-                                </p>
                                 </div>
                                 </div>
                                 </div>
               </div>
-            </div>
             );
 }
 
