006649ef52a31dfde6f349c57f9665b64e375871
Daniel Schauer <dschauer17@gmail.com>
2019-04-04 20:57:20 -0400

    nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,6 +6,13 @@ function Nav() {
         <nav>
             <div className="nav-wrapper">
                 <a href="google.com" className="brand-logo">Space!</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="Home.js">Home</a></li>
+        <li><a href="Hubble.js">Hubble</a></li>
+        <li><a href="Media.js">Media</a></li>
+        <li><a href="Planets.js">Planets</a></li>
+        <li><a href="Rover.js">Rover</a></li>
+      </ul>
             </div>
         </nav>
     </div>
