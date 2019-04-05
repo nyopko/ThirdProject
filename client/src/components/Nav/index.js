ac269c442523710c2d12d6bf5479e6a2830dc080
jphillips37 <42793705+jphillips37@users.noreply.github.com>
2019-04-04 21:02:19 -0400

    Update index.js
    changed .js links route links

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,11 +7,11 @@ function Nav() {
             <div className="nav-wrapper">
                 <a href="google.com" className="brand-logo">Space!</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="Home.js">Home</a></li>
-        <li><a href="Hubble.js">Hubble</a></li>
-        <li><a href="Media.js">Media</a></li>
-        <li><a href="Planets.js">Planets</a></li>
-        <li><a href="Rover.js">Rover</a></li>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/hubble">Hubble</a></li>
+        <li><a href="/media">Media</a></li>
+        <li><a href="/planets">Planets</a></li>
+        <li><a href="/rover">Rover</a></li>
       </ul>
             </div>
         </nav>
