9bbc47ac1040c40c7b0a5e31312673e3d8162bdb
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-04-06 14:59:45 -0400

    Have a mars image showing

--- client/src/components/SidebarWrapper/index.js
+++ client/src/components/SidebarWrapper/index.js
@@ -5,21 +5,62 @@ import Daily from '../Daily';
 import './style.css';
 import API from "../../utils/API";
 import M from 'materialize-css';
+import axios from "axios"
 require("dotenv").config();
 
 class SidebarWrapper extends Component {
 
-  state = {
-    result: {}
-  };
+  constructor(props){
+    super(props);
+
+    this.state = {
+      roverChosen: "curiosity",
+      cameraChosen: "",
+      firstResponsePhoto: null,
+      error: "",
+      loading: "",
+      initialRequestMade: false
+    };
+  }
+
+
 
     componentDidMount() {
         this.dailyPull();
-        document.addEventListener('DOMContentLoaded', function() {
-          var elems = document.querySelectorAll('.collapsible');
-          var options = {}
-          var instances = M.Collapsible.init(elems, options);
-        });
+        const APIKEY = process.env.REACT_APP_API_KEY;
+        let randomValue = Math.floor(Math.random() * 2000)
+const marsURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=" + randomValue + "&page=1&api_key=" + APIKEY
+
+axios.get(marsURL)
+.then((res) => {
+  if (res.data.Error) { // if error
+    this.setState({
+      error: res.errors
+    });
+  } else { // if success
+    let photoVal = "";
+
+    // check if any photos come back
+    if (!!res.data.photos[0]) {
+      photoVal = res.data.photos[0].img_src;
+    } else {
+      photoVal = null;
+    }
+
+    // regardless of if photos come back:
+    this.setState({
+      firstResponsePhoto: photoVal,
+      loading: "",
+      initialRequestMade: true
+    });
+  }
+});
+
+        // document.addEventListener('DOMContentLoaded', function() {
+        //   var elems = document.querySelectorAll('.collapsible');
+        //   var options = {}
+        //   var instances = M.Collapsible.init(elems, options);
+        // });
       }
 
     dailyPull () {
@@ -33,9 +74,9 @@ class SidebarWrapper extends Component {
       <div className="sidebar">
         <NEO></NEO>
         <Daily 
-        title={this.state.result.title}
-        url={this.state.result.url}
-        explanation={this.state.result.explanation}
+        // title={this.state.result.title}
+        url={this.state.firstResponsePhoto}
+        // explanation={this.state.result.explanation}
         />
             </div>
             );
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,14 +1,23 @@
 import axios from "axios";
 require('dotenv').config();
+
+/// APOD 
 const apodURL = "https://api.nasa.gov/planetary/apod?api_key=";
 const APIKEY = process.env.REACT_APP_API_KEY;
+
+/// MARS
+let randomValue = Math.floor(Math.random() * 2000)
+const marsURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=" + randomValue + "&page=1&api_key=" + APIKEY
 
 
 export default {
   grab: function() {
-    console.log(process.env.REACT_APP_API_KEY);
+    console.log(marsURL);
     console.log(APIKEY);
     console.log(apodURL);
-    return axios.get(apodURL + APIKEY);
+   return axios.get(marsURL)
+  //  .then(data => {
+  //    console.log(data.data.photos[0].img_src)
+  //   })
   }
 };
