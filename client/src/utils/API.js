086d2b01541b59da3899699f83682a5038194c82
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-04-06 11:18:44 -0400

    Made api call for asteroids, installed moment.js node module, have working link

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,12 +1,20 @@
 import axios from "axios";
+import moment from "moment";
 require('dotenv').config();
+
+/// Image of the day
 const apodURL = "https://api.nasa.gov/planetary/apod?api_key=";
 const APIKEY = process.env.REACT_APP_API_KEY;
+
+/// Asteroids / NEO
+let date = moment("1995-09-13", "YYYY-MM-DD");
+let dateFormatted = date._i
+const neoURL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + dateFormatted + "&end_date="+ dateFormatted + "&api_key=" + APIKEY;
 
 
 export default {
   grab: function() {
-    console.log(process.env.REACT_APP_API_KEY);
+    console.log(neoURL);
     console.log(APIKEY);
     console.log(apodURL);
     return axios.get(apodURL + APIKEY);
