560cc5028e3b67be5bcf1aeed571f5ed9d00791d
John Phillips <jphillips37@gmail.com>
2019-04-09 19:52:54 -0400

    added logic to check for existing user on user create

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -4,14 +4,29 @@ const favsController = require("../../controllers/controller.js");
 router.route("/new_user")
     .post(favsController.addUser);
 
-router.route("/new_fav")
-    .post(favsController.addFav);
+router.route("/new_video")
+    .post(favsController.addFavVideo);
+
+router.route("/new_audio")
+    .post(favsController.addFavAudio);
+
+router.route("/new_image")
+    .post(favsController.addFavImage);
 
 router.route("/get_user")
     .get(favsController.findUser);
 
 router.route("/:user/favorites")
     .get(favsController.findItem);
+
+router.route("/:user/videos")
+    .get(favsController.findVideos);
+
+router.route("/:user/audio")
+    .get(favsController.findAudio);
+
+router.route("/:user/images")
+    .get(favsController.findImages);
 
 router.route("/:user/delete")
     .delete(favsController.remove);
