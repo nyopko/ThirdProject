d795ca15cad77bc3368166af0efd56ed23d9eb34
John Phillips <jphillips37@gmail.com>
2019-04-09 21:05:25 -0400

    changed database layout to have a single URL column and a media_type column. updated controller.js, routes file, and seeds.sql files to reflect these changes.

--- routes/api/apiRoutes.js
+++ routes/api/apiRoutes.js
@@ -4,29 +4,14 @@ const favsController = require("../../controllers/controller.js");
 router.route("/new_user")
     .post(favsController.addUser);
 
-router.route("/new_video")
-    .post(favsController.addFavVideo);
-
-router.route("/new_audio")
-    .post(favsController.addFavAudio);
-
-router.route("/new_image")
-    .post(favsController.addFavImage);
+router.route("/add_url")
+    .post(favsController.addFavUrl);
 
 router.route("/get_user")
     .get(favsController.findUser);
 
 router.route("/:user/favorites")
-    .get(favsController.findItem);
-
-router.route("/:user/videos")
-    .get(favsController.findVideos);
-
-router.route("/:user/audio")
-    .get(favsController.findAudio);
-
-router.route("/:user/images")
-    .get(favsController.findImages);
+    .get(favsController.findUrls);
 
 router.route("/:user/delete")
     .delete(favsController.remove);
