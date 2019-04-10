7a6184597830f2e910872552ba11f2114159f43b
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-04-09 21:17:20 -0400

    Got name of near earth object to show. Will add more details later

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,16 @@
 import axios from "axios";
+import moment from "moment";
 require('dotenv').config();
 
 /// APOD 
 const APIKEY = process.env.REACT_APP_API_KEY;
 const apodURL = "https://api.nasa.gov/planetary/apod?api_key=" + APIKEY;
+
+/// Asteroids / NEO
+let dateFormatted = moment();
+dateFormatted = moment(dateFormatted).format("YYYY-MM-DD");
+
+const neoURL = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + dateFormatted + "&end_date="+ dateFormatted + "&api_key=" + APIKEY;
 
 
 export default {
@@ -11,5 +18,11 @@ export default {
     console.log(APIKEY);
     console.log(apodURL);
    return axios.get(apodURL)
-  }
+  },
+
+  grabAPOD: function() {
+    console.log(neoURL);
+    return axios.get(neoURL)
+
+}
 };
