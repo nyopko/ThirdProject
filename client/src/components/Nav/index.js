ce0accd93180f42fe50a9a7487a010dfe7e41342
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-04-11 19:49:48 -0400

    Created login and got media api running as expected

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 
+
 function Nav() {
     return <div className="nav">
         <nav>
--- client/src/pages/Rover.js
+++ client/src/pages/Rover.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import RoverWrapper from '../components/RoverWrapper';
+import Login from '../components/Login';
 
 
 
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="roverPage">
+      <Login />
       <Header />
       <RoverWrapper />
       </div>
