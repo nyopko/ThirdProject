18f999f91b9441dbed12bacabed13f7dceabc066
Alberto Balsalm <44181737+nyopko@users.noreply.github.com>
2019-04-13 16:49:44 -0400

    Fixed typos, added header to resources page

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
 import ResourcesContent from '../components/ResourcesContent/';
 
 
@@ -7,7 +6,19 @@ class App extends Component {
     render() {
       return (
         <div className="resourcesPage">
-        <Header />
+        <div className="main">
+          <div className="row">
+            <div className="col s12 m9">
+              <div className="row">
+                <div className="col s12">
+                  <div className="card-panel">
+                    <h2 className="center-align">Resources</h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <ResourcesContent />
         </div>
       );
