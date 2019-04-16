a6ee3f4e8e4e36cfdefd9e05596f4f2e9ef5750e
Stacy Acker <stacyacker@Stacys-MacBook-Pro.local>
2019-04-16 16:00:41 -0400

    added startic handling to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const db = require("./models");
+const path = require("path");
 
 //const routes = require("./routes");
 const controller = require("./controllers/controller.js");
@@ -13,6 +14,8 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'client/build')));
 
 
 app.use(routes);
